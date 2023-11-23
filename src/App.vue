7d5cf1b3a44c1776ee1ec852a154035795bd1af3
Jimmy García <jimmyegc@gmail.com>
2023-11-23 16:50:09 -0600

    proyecto inicial

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<script setup>
+import { ref } from 'vue'
+import DropZone from './components/DropZone.vue'
+
+let dropzoneFile = ref("")
+let previewImage = ref("")
+
+const drop = (e) => {
+  dropzoneFile.value = e.dataTransfer.files[0]
+  previewImage = URL.createObjectURL(dropzoneFile.value)
+}
+
+const selectedFile = () => {
+  dropzoneFile.value = document.querySelector('.dropzoneFile').files[0]
+  previewImage = URL.createObjectURL(dropzoneFile.value)  
+}
+
+</script>
+
+<template>  
+  <div class="home">
+    <h1>DropZone</h1>
+    <DropZone @drop.prevent="drop" @change="selectedFile" />
+    <span class="file-info">File: {{ dropzoneFile.name }}</span>      
+    <img :src="previewImage" alt="">
+  </div>  
+</template>
+
+<style scoped lang="scss">
+.home {
+  height: 100vh; 
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #f1f1f1;
+  
+  h1 {
+    font-size: 40px;
+    margin-bottom: 32px;
+  }
+  .file-info {
+    margin-top: 32px;
+  }
+}
+
+</style>
---
+++ src/components/DropZone.vue
@@ -0,0 +1,62 @@
+<script setup>
+import { ref } from 'vue'
+const active = ref(false)
+const toggleActive = () => {
+  active.value = !active.value  
+}
+</script>
+
+<template>
+  <div 
+    @dragenter.prevent="toggleActive" 
+    @dragleave.prevent="toggleActive"
+    @dragover.prevent
+    @drop.prevent="toggleActive"
+    :class="{ 'active-dropzone': active }"
+    class="dropzone">
+    <span>Drag of Drop File</span>
+    <span>OR</span>
+    <label for="dropzoneFile">Select File</label>
+    <input type="file" id="dropzoneFile" class="dropzoneFile" >    
+  </div>  
+</template>
+
+<style scoped lang="scss">
+.dropzone {
+  width: 400px;
+  height: 200px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  row-gap: 16px;
+  border: 2px dashed #41b883;
+  background-color: white;
+  transition: .3s ease all;
+  
+  label {
+    padding: 8px 12px;
+    color: #fff;
+    background-color:  #41b883;
+    transition: .3s ease all;
+  }
+
+  input { 
+    display: none;
+  }
+
+}
+
+.active-dropzone {
+  color: #fff;
+  border-color: #fff;
+  background-color: #41b883;
+  
+  label {
+    background-color: #FFF;
+    color: #41b883;
+  }
+}
+
+</style>
+
